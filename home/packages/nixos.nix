c46b06200c07827968707ad36fca41edf675a9cc
Z4que <114612941+Zaque-69@users.noreply.github.com>
2025-06-27 23:48:33 +0300

    Update nixos.nix

--- home/packages/nixos.nix
+++ home/packages/nixos.nix
@@ -1,6 +1,7 @@
 { pkgs, ... } : {
     home.packages = with pkgs; [
         nix-init                            # Command line tool to generate Nix packages from URLs
+        nix-ld                              # Run unpatched dynamic binaries on NixOS
         nix-prefetch-git                    # Script used to obtain source hashes for fetchgit
         nix-template                        # Make creating nix expressions easy
     ];
